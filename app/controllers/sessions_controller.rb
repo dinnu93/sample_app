8540b800677ffc074e8b096162b5d36cecf6a175
Dinesh Padisetti <dinesh.padisetti@gmail.com>
2017-02-16 11:24:17 +0530

    Added followers and feed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
         flash[:success] = 'Logged in successfully!'
   		  log_in user
   		  params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  		  redirect_back_or user
+  		  redirect_back_or root_url
       else
         message = "Account not activated. "
         message += "Check you email for the activation link."
